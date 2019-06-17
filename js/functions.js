7ebe0a9f6d83e343d7e98754cd6ab446e9efb158
Benedict Wong <benatparapet@hotmail.com>
2019-06-16 21:01:54 -0700

    changes to the detailed product page

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,21 @@
-function pointTo(url) {
-  location.href=url;
+function showImage(x){
+  if(x == "1"){
+
+    document.querySelector('.image-preview').src = "img/item_1.png"
+    document.querySelector('.image-preview').alt = "image 1 preview"
+
+  }
+  if(x == "2"){
+
+    document.querySelector('.image-preview').src = "img/item_2.png"
+    document.querySelector('.image-preview').alt = "image 2 preview"
+
+  }
+  if(x == "3"){
+
+    document.querySelector('.image-preview').src = "img/item_3.png"
+    document.querySelector('.image-preview').alt = "image 3 preview"
+
+  }
+
 }
