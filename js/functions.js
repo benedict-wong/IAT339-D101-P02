bd738e86ea9a853f2ae3b378532dd587477854f3
Benedict Wong <benatparapet@hotmail.com>
2019-06-16 22:04:03 -0700

    added image content and changes to review section

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,19 @@
 function showImage(x){
   if(x == "1"){
 
-    document.querySelector('.image-preview').src = "img/item_1.png"
+    document.querySelector('.image-preview').src = "img/item_1_0.png"
     document.querySelector('.image-preview').alt = "image 1 preview"
 
   }
   if(x == "2"){
 
-    document.querySelector('.image-preview').src = "img/item_2.png"
+    document.querySelector('.image-preview').src = "img/item_1_1.png"
     document.querySelector('.image-preview').alt = "image 2 preview"
 
   }
   if(x == "3"){
 
-    document.querySelector('.image-preview').src = "img/item_3.png"
+    document.querySelector('.image-preview').src = "img/item_1_2.png"
     document.querySelector('.image-preview').alt = "image 3 preview"
 
   }
