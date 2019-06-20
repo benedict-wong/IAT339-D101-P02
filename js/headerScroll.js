5fdd6c8e09b995f6c04ea2cd034cbca2e44a288c
Benedict Wong <benatparapet@hotmail.com>
2019-06-20 14:09:20 -0700

    added headerscroll to counteract body ratio being too small in landscape

---
+++ js/headerScroll.js
@@ -0,0 +1,13 @@
+// implemented using reference to https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+  var lastScrollPos = window.pageYOffset;
+  window.onscroll = function() {
+  var currScrollPos = window.pageYOffset;
+    if (lastScrollPos > currScrollPos) {
+      document.getElementById("header").style.top = "0";
+      document.querySelector(".main-content").style.paddingTop = "12em";
+    } else {
+      document.getElementById("header").style.top = "-15em";
+      document.querySelector(".main-content").style.paddingTop = "2em";
+    }
+    lastScrollPos = currScrollPos;
+  }
