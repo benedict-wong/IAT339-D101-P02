d1a71e2247c67e0caa00c80140cbc8b493007e83
Benedict Wong <benatparapet@hotmail.com>
2019-06-21 16:27:36 -0700

    fixed header not returning after scroll up/added smooth transition.

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,15 +1,14 @@
 // implemented using reference to https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
-if(window.innerHeight < window.innerWidth){
-  var lastScrollPos = window.pageYOffset;
-  window.onscroll = function() {
-  var currScrollPos = window.pageYOffset;
-    if (lastScrollPos > currScrollPos) {
-      document.getElementById("header").style.top = "0";
-      document.querySelector(".main-content").style.paddingTop = "12em";
-    } else {
-      document.getElementById("header").style.top = "-15em";
-      document.querySelector(".profile-right").style.paddingTop = "";
-    }
-    lastScrollPos = currScrollPos;
+
+var lastScrollPos = window.pageYOffset;
+window.onscroll = function() {
+var currScrollPos = window.pageYOffset;
+  if (lastScrollPos > currScrollPos) {
+    document.getElementById("header").style.top = "0";
+    document.querySelector(".main-content").style.paddingTop = "12em";
+  } else {
+    document.getElementById("header").style.top = "-15em";
+    document.querySelector(".main-content").style.paddingTop = "5em";
   }
+  lastScrollPos = currScrollPos;
 }
