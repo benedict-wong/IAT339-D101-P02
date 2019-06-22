58bb7246d487c9a44a220996787423df1a26ce38
Benedict Wong <benatparapet@hotmail.com>
2019-06-22 00:15:26 -0700

    bug fixed from issues.txt

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,14 +1,20 @@
 // implemented using reference to https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 
-var lastScrollPos = window.pageYOffset;
-window.onscroll = function() {
-var currScrollPos = window.pageYOffset;
-  if (lastScrollPos > currScrollPos) {
-    document.getElementById("header").style.top = "0";
-    document.querySelector(".main-content").style.paddingTop = "12em";
-  } else {
-    document.getElementById("header").style.top = "-15em";
-    document.querySelector(".main-content").style.paddingTop = "5em";
+var h = parseInt(window.outerHeight) *2;
+console.log(window.outerWidth);
+
+
+if(h < window.innerWidth){
+  var lastScrollPos = window.pageYOffset;
+  window.onscroll = function() {
+  var currScrollPos = window.pageYOffset;
+    if (lastScrollPos > currScrollPos) {
+      document.getElementById("header").style.top = "0";
+      document.querySelector(".main-content").style.paddingTop = "12em";
+    } else {
+      document.getElementById("header").style.top = "-15em";
+      document.querySelector(".main-content").style.paddingTop = "5em";
+    }
+    lastScrollPos = currScrollPos;
   }
-  lastScrollPos = currScrollPos;
 }
