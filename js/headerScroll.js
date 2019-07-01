2d8b37e1544af6946320511def75970f7c24a609
Benedict Wong <benatparapet@hotmail.com>
2019-06-30 18:16:45 -0700

    semantic html5 changes

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,20 +1,22 @@
 // implemented using reference to https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 
-var h = parseInt(window.outerHeight) *2;
-console.log(window.outerWidth);
 
 
-if(h < window.innerWidth){
-  var lastScrollPos = window.pageYOffset;
-  window.onscroll = function() {
-  var currScrollPos = window.pageYOffset;
-    if (lastScrollPos > currScrollPos) {
-      document.getElementById("header").style.top = "0";
-      document.querySelector(".main-content").style.paddingTop = "12em";
-    } else {
-      document.getElementById("header").style.top = "-15em";
-      document.querySelector(".main-content").style.paddingTop = "5em";
+if(window.outerWidth <=800 && window.outerHeight <= 600){
+    var h = parseInt(window.outerHeight) *1.5;
+
+    if(h < window.innerWidth){
+      var lastScrollPos = window.pageYOffset;
+      window.onscroll = function() {
+      var currScrollPos = window.pageYOffset;
+        if (lastScrollPos > currScrollPos) {
+          document.getElementById("header").style.top = "0";
+          document.querySelector(".main-content").style.paddingTop = "12em";
+        } else {
+          document.getElementById("header").style.top = "-15em";
+          document.querySelector(".main-content").style.paddingTop = "5em";
+        }
+        lastScrollPos = currScrollPos;
+      }
     }
-    lastScrollPos = currScrollPos;
-  }
 }
